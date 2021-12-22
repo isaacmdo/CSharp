18cdf5ee2b66a570907ba8dc14a4de236d551b85
Isaac <isaacestudo1999@gmail.com>
2021-12-21 22:55:29 -0300

    ...

---
+++ Moedas/Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 10536.25m;
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("en-US")
+                ));
+            Console.WriteLine(valor.ToString(
+                "C",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(valor.ToString(
+                "N",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(valor.ToString(
+                "P",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(valor.ToString(
+                "F",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(valor.ToString(
+                "G",
+                CultureInfo.CreateSpecificCulture("pt-BR")));
+        }
+    }
+}
